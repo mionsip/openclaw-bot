5800e2f386e438839a4a1396ddd8095a1d630c09
mionsip <buiquan0906@gmail.com>
2026-05-01 09:17:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-bot.on("message", (msg) => {
-  if (msg.text === "/start") {
-    bot.sendMessage(msg.chat.id, "Xin chào 😎");
-  } else {
-    bot.sendMessage(msg.chat.id, "Bạn nói: " + msg.text);
-  }
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+
+const app = express();
+
+// giữ Railway sống
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(process.env.PORT || 3000);
+
+// ===== TELEGRAM BOT =====
+const bot = new TelegramBot(process.env.TOKEN, {
+  polling: true
+});
+
+// xử lý lệnh /start
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Xin chào 😎");
 });
