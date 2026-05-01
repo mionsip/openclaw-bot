70f45a2ba592654029962a2567605317fb9b890e
mionsip <buiquan0906@gmail.com>
2026-05-01 18:21:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-import express from "express";
-
-const app = express();
-app.use(express.json());
-
-// fake webhook nhận tin nhắn
 app.post("/webhook", (req, res) => {
-  console.log("Tin nhắn:", req.body);
+  console.log("Zalo message:", req.body);
 
-  // luôn trả lời cố định
-  console.log("Reply: Đợi một chút tôi đang bận nhé!!!");
-
-  res.sendStatus(200);
+  return res.json({
+    reply: "Đợi một chút tôi đang bận nhé!!!"
+  });
 });
-
-// keep alive cho UptimeRobot
-app.get("/", (req, res) => {
-  res.send("BOT OK");
-});
-
-app.listen(process.env.PORT || 3000);
