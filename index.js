0be0fb6de25565afe756c751d75e1b487befabe1
mionsip <buiquan0906@gmail.com>
2026-05-01 09:15:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const express = require("express");
-const TelegramBot = require("node-telegram-bot-api");
-
-const app = express();
-
-// giữ cho Railway không sleep ngay
-app.get("/", (req, res) => {
-  res.send("Bot is running");
-});
-
-app.listen(process.env.PORT || 3000);
-
-// ===== TELEGRAM BOT =====
-const bot = new TelegramBot(process.env.TOKEN, {
-  polling: true
-});
-
-bot.on("message", (msg) => {
-  bot.sendMessage(msg.chat.id, "Bot đang chạy 🚀");
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Xin chào 😎");
 });
