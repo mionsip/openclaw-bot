2d351dea675668c34ff94a50d69c6ab03055c704
mionsip <buiquan0906@gmail.com>
2026-05-01 16:54:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ export default async function onMessage(msg, { sendMessage }) {
     console.error(err);
   }
 }
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive");
+});
+
+app.listen(process.env.PORT || 3000);
